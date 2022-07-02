f0d7dead567d215ab05381812c12304911153b98
freetime <43937423+BeanGTA@users.noreply.github.com>
2022-07-02 11:11:53 +0700

    Update lineage_alphaplus.mk

--- lineage_alphaplus.mk
+++ lineage_alphaplus.mk
@@ -21,7 +21,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit from alphaplus device
-$(call inherit-product, device/lge/alphalm/device.mk)
+$(call inherit-product, device/lge/alphaplus/device.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
