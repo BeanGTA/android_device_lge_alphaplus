032293d6e040655d1be37f20b1005167beaeedb2
freetime <43937423+BeanGTA@users.noreply.github.com>
2022-07-02 11:15:46 +0700

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -25,6 +25,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter alphalm,$(TARGET_DEVICE)),)
+ifneq ($(filter alphaplus,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
